a5bc0ec6a58ab182c4049c625df963f22e32eb22
Eugene Kazakov <ekazakov@parallels.com>
2014-07-21 10:21:41 +0700

    Add copyrights.

--- plib/resources/locales/en-US.php
+++ plib/resources/locales/en-US.php
@@ -1,5 +1,5 @@
 <?php
-
+// Copyright 1999-2014. Parallels IP Holdings GmbH. All Rights Reserved.
 $messages = [
     'pageTitle' => 'Syslog Watch',
     'lines' => 'Lines of log file to be displayed (from the end of the file)',
