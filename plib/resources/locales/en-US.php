2207d6077308f15f41a1cf4c22beff0aa8bbbb96
Alexei Yuzhakov <ayuzhakov@parallels.com>
2015-03-22 20:15:10 +0600

    Update copyright year.

--- plib/resources/locales/en-US.php
+++ plib/resources/locales/en-US.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright 1999-2014. Parallels IP Holdings GmbH.
+// Copyright 1999-2015. Parallels IP Holdings GmbH.
 $messages = [
     'pageTitle' => 'Syslog Watch',
     'lines' => 'Lines of log file to be displayed (from the end of the file)',
